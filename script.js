fc163a0d80c79801adc87f9468bd67df036cbda6
aliali3356 <mrehan4157@gmail.com>
2026-07-22 15:35:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const vapi = new Vapi("aa8556ed-899a-45ca-be9c-94c6da221858");
-
-const button = document.getElementById("start");
-
-button.onclick = async () => {
-  console.log("Button clicked");
-
-  try {
-    await vapi.start("5cf2c58a-db0c-4430-96a0-ee9f120e7be8");
-    console.log("Call started");
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
+<script>
+  var vapiWidgetConfig = {
+    apiKey: 'aa8556ed-899a-45ca-be9c-94c6da221858', // Replace with your Public Key
+    assistantId: '5cf2c58a-db0c-4430-96a0-ee9f120e7be8', // Replace with your Assistant ID
+    config: {
+      // Customize UI options (optional)
+      title: 'Voice Assistant',
+      position: 'bottom-right',
+    }
+  };
+</script>
+<script src="https://vapi.ai" defer></script>
